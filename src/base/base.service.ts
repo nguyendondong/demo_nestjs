105d31b9e9b0bb8658eac1cf7668c6e562346cdc
“nguyen.don.dong” <nguyendondong.dev@gmail.com>
2024-03-13 01:40:34 +0700

    apply pagination

--- src/base/base.service.ts
+++ src/base/base.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from "@nestjs/common";
 import { InjectEntityManager } from "@nestjs/typeorm";
 
-import { EntityManager, EntityTarget } from "typeorm";
+import { EntityManager, EntityTarget, FindManyOptions } from "typeorm";
 import { PaginationDto } from "@/base/dto/pagination.dto";
+import { FindOneOptions } from "typeorm/find-options/FindOneOptions";
+import { FindOptionsOrder } from "typeorm/find-options/FindOptionsOrder";
 
 @Injectable()
 export class BaseService<Entity> {
@@ -14,11 +16,16 @@ export class BaseService<Entity> {
     entity: EntityTarget<Entity>,
     paginationDto: PaginationDto
   ): Promise<any> {
+    const options: FindOptionsOrder<any> = {
+      email: "ASC",
+      createdAt: "DESC",
+    };
     const page = Number(paginationDto.page) || 1;
     const limit = Number(paginationDto.limit) || 10;
     const [entities, count] = await this.entityManager.findAndCount(entity, {
       skip: (page - 1) * limit,
       take: limit || 10,
+      order: options,
     });
 
     const lastPage = Math.ceil(count / limit);
